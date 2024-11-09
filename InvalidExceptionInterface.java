029990c077e8da5bd38a6533e35bc373395aa3b6
tinexu <121458001+tinexu@users.noreply.github.com>
2024-11-09 16:34:14 -0500

    Update and rename Interface for the InvalidException class to InvalidExceptionInterface.java

---
+++ InvalidExceptionInterface.java
@@ -0,0 +1,13 @@
+/**
+ * Group Project -- InvalidExceptionInterface
+ * <p>
+ * This class is the interface for our InvalidException class
+ *
+ * @author L30-Team 1, CS180
+ * @version Nov 3, 2024
+ */
+
+// Interface that our Custom Exception, InvalidException will implement
+public interface InvalidExceptionInterface {
+
+}
