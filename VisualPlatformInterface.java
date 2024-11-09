1fc0c809d105d0e077a9e370d76d95ae76005b68
tinexu <121458001+tinexu@users.noreply.github.com>
2024-11-09 16:35:40 -0500

    Update and rename Interface for the VisualPlatform class to VisualPlatformInterface.java

---
+++ VisualPlatformInterface.java
@@ -0,0 +1,13 @@
+/**
+ * Group Project -- VisualPlatformInterface
+ * <p>
+ * This class is the interface for the VisualPlatform class
+ *
+ * @author L30-Team 1, CS180
+ * @version Nov 3, 2024
+ */
+
+// Interface that our VisualPlatform class will implement
+public interface VisualPlatformInterface {
+
+}
