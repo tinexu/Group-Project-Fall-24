3226b2b57cdb8615b61b1822e708751eec7cc57c
srinidhisarav <sarava32@purdue.edu>
2024-11-17 23:32:03 -0500

    Create ServerInterface.java

---
+++ ServerInterface.java
@@ -0,0 +1,4 @@
+public Interface ServerInterface {
+  void defaultCommandsForWriter(PrintWriter w, String input); // A method to just writes the input and flushes the writer automatically
+  void run(); // The run method for the Server class
+}
